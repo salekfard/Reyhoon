d68fe2f2e2fc67dc63fbda25f0e05f7745688e8c
Ali Salekfard <ali.salekfard@gmail.com>
2018-02-02 12:23:29 +0330

    Impliment Base MainViewController Storyboard

---
+++ Reyhoon/Classes/ConstHelper.swift
@@ -0,0 +1,17 @@
+//
+//  ConstHelper.swift
+//  Reyhoon
+//
+//  Created by Ali Salekfard on 2/2/18.
+//  Copyright © 2018 Ali Salekfard. All rights reserved.
+//
+
+import UIKit
+
+let IS_IPAD = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad);
+
+let GENERAL_URL_SERVICES : String = "http://stage.reyhoon.com";
+
+class ConstHelper: NSObject {
+
+}
